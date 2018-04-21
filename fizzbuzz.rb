f4bc907d3f10d017322e079249156581f802a9d2
Shelly Chen <shellyjychen@gmail.com>
2018-04-21 06:09:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(x)
-  if x % 3 == 0
+def fizzbuzz(int)
+  if int % 3 == 0
     puts "Fizz"
 
-  elsif x % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
 
-  elsif x % 15 == 0
+  elsif int % 15 == 0
     puts "FizzBuzz"
 
   else
